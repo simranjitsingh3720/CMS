49a4aff0b25e163eec181e2d741921d25a3f5694
Aditya Phani <adityaphani@Adityas-MacBook-Air-2.local>
2022-02-02 12:49:01 +0530

    message

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,25 +1,29 @@
 import db from "../../db/models";
 
 const handler = async (req, res) => {
-  const jane = await db.Schema.create({
-    slug: "articles",
-    schema: { name: "mohit" },
-    createdBy: "83b99f21-1fd4-41ee-9616-7642713f7f24",
-  });
+  // const jane = await db.Schema.create({
+  //   slug: "articles",
+  //   schema: { name: "phani" },
+
+  //   createdBy: "410be22b-0583-4753-bda3-c3a2e028bd64",
+  // });
 
   // const jane = await db.User.create({
-  //   email:"as@gmail.com",
-  //   password:"ass",
-  //   name:"sfff"
+  //   email:"phanipilla@gmail.com",
+  //   password:"qwertyuiop",
+  //   // name:"sfff"
+  //   firstName:"phani",
+  //   lastName:"pilla"
+
   // });
 
-  // const jane = await db.Schema.findAll();
-  // const users = await db.User.findAll({
-  //   where: {
-  //     id: jane[0].created_by,
-  //   },
-  // });
-  res.status(200).json({ name: jane });
+  const jane = await db.Schema.findAll();
+  const users = await db.User.findAll({
+    where: {
+      id: jane[0].createdBy,
+    },
+  });
+  res.status(200).json({ name: users });
 };
 
 export default handler;
