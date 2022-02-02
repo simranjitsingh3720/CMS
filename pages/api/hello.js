ea3bd5561c5505c57a07c36a9f7e00805832744c
Rahul Kumar <rahulk@cogoport.com>
2022-02-02 12:50:34 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,25 +1,28 @@
 import db from "../../db/models";
 
 const handler = async (req, res) => {
-  const jane = await db.Schema.create({
-    slug: "articles",
-    schema: { name: "mohit" },
-    createdBy: "83b99f21-1fd4-41ee-9616-7642713f7f24",
-  });
+  // const jane = await db.Schema.create({
+  //   slug: "articles",
+  //   schema: { name: "Rahul" },
+  //   createdBy: "dad6dcf0-3549-45c9-8f00-f45ce032d8f2",
+  // });
 
   // const jane = await db.User.create({
-  //   email:"as@gmail.com",
-  //   password:"ass",
-  //   name:"sfff"
+  //   email:"ankit@gmail.com",
+  //   firstName:'Ankit',
+  //   lastName:'Kaushal',
+  //   password:"password",
   // });
 
-  // const jane = await db.Schema.findAll();
-  // const users = await db.User.findAll({
-  //   where: {
-  //     id: jane[0].created_by,
-  //   },
-  // });
-  res.status(200).json({ name: jane });
+const jane = await db.Schema.findAll();
+
+const users = await db.User.findAll({
+    where: {
+      id: jane[0].createdBy,
+    },
+  });
+
+  res.status(200).json({ name: users });
 };
 
 export default handler;
