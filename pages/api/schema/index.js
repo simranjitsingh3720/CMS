5d60389a5f190c8bae6fa78e9140c5dd0ddda416
Simranjit Singh <simranjitsingh@Simranjits-MacBook-Air.local>
2022-02-08 13:32:42 +0530

    add schma routes and controllers

--- pages/api/schema/index.js
+++ pages/api/schema/index.js
@@ -1,6 +1,16 @@
+const { listSchemas, addSchema } = require('../../../api-controllers/schema-controller');
 
-const handler = async (req, res) => {
-  res.status(200).json({ name: "schema" });
+const schemaHandler = async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      return listSchemas(req, res);
+
+    case 'POST':
+      return addSchema(req, res);
+
+    default:
+      return '';
+  }
 };
 
-export default handler;
+export default schemaHandler;
