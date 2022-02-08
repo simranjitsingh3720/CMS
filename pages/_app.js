ee81f25c829b4ffdefd17ff8ccd45384461708f5
Simranjit Singh <simranjitsingh@Simranjits-MacBook-Air.local>
2022-02-08 15:54:52 +0530

    add sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import 'antd/dist/antd.css';
 
+import styles from '../styles/globals.module.scss';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- ui/page-components/Home/index.js
+++ ui/page-components/Home/index.js
@@ -1,5 +1,7 @@
+import styles from '../../../styles/Home.module.scss';
+
 function Home() {
-  return <div>Hello world</div>;
+  return <div className={styles.design}>Hello world</div>;
 }
 
 export default Home;
