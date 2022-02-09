aaeeb913e1b4fee0f476a5272ba9e6c64e2ca1db
Aditya Phani <adityaphani@Adityas-MacBook-Air-2.local>
2022-02-10 00:26:53 +0530

    commit4

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import 'antd/dist/antd.css';
-<<<<<<< HEAD
-import './styles/asset-styles.css'
-import './styles/form-styles.css'
 
-import styles from '../styles/globals.scss';
-=======
+import './styles/asset-styles.css';
+import './styles/form-styles.css';
+
 import Head from 'next/head';
+import styles from '../styles/globals.scss';
 import PageLayout from '../ui/page-components/components/layout/PageLayout';
-import '../styles/globals.scss';
->>>>>>> b2160438394f86a2b759e9b1d5585e8300548fee
 
 function MyApp({ Component, pageProps }) {
   const { title: propTitle } = pageProps || {};
