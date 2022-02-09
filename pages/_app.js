5b15eec6d6509aba563a7f123b662c15211b3162
Rudraprasad Das <brayn003@gmail.com>
2022-02-09 20:43:51 +0530

    unecessary styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,8 @@
 import 'antd/dist/antd.css';
-<<<<<<< HEAD
-import './styles/asset-styles.css'
-import './styles/form-styles.css'
+import '../ui/globals.scss';
 
-import styles from '../styles/globals.scss';
-=======
 import Head from 'next/head';
 import PageLayout from '../ui/page-components/components/layout/PageLayout';
-import '../styles/globals.scss';
->>>>>>> b2160438394f86a2b759e9b1d5585e8300548fee
 
 function MyApp({ Component, pageProps }) {
   const { title: propTitle } = pageProps || {};
