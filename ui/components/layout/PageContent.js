56c7fdea2c611c3d0b0fb63cd957e68072ae56d4
mohitCogoport <mohit.raj@cogoport.com>
2022-02-10 15:49:18 +0530

    folder str

--- ui/components/layout/PageContent.js
+++ ui/components/layout/PageContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Content } from 'antd/lib/layout/layout';
 import { useRouter } from 'next/router';
-import PageTitle from '../PageTitle/PageTitle';
+import PageTitle from '../PageTitle';
 
 const blockRoute = ['/', '/admin/signin', '/admin/signup'];
 
