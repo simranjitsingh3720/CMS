394d6d18c9ba3d0dea7c3a7524e26fa13ced5699
Rahul Kumar <rahulk@cogoport.com>
2022-02-16 17:41:46 +0530

    pull

--- ui/page-components/page-manager/PageBuilder/index.js
+++ ui/page-components/page-manager/PageBuilder/index.js
@@ -4,61 +4,182 @@ import 'grapesjs/dist/css/grapes.min.css';
 import GrapesJS from 'grapesjs';
 import gjsPresetWebpage from 'grapesjs-preset-webpage';
 import axios from 'axios';
+import useAxios from 'axios-hooks';
 
 function PageBuilder() {
   const [editor, setEditor] = useState(null);
+  const [pageData, setPageData] = useState('');
   const router = useRouter();
-  const getApi = () => {
+
+  // useEffect(() => {
+  //   if (getData) {
+  //     console.log(getData.data.data);
+  //     setPageData(getData.data.data);
+  //   }
+  // }, [getData]);
+
+  // const getApiN = () => {
+  //   console.log(getData);
+  //   if (router.query.pageSlug) {
+  //     console.log(pageData);
+  //     console.log(getData);
+  //     const obj = null;
+
+  //     let LandingPage = {};
+  //     // obj = JSON.parse(pageData);
+  //     LandingPage = {
+  //       html: obj && obj['CMS-html'],
+  //       css: obj && obj['CMS-css'],
+  //       components: obj && obj['CMS-components'],
+  //       style: obj && obj['CMS-styles'],
+  //     };
+
+  //     if (!editor) {
+  //       const e = GrapesJS.init({
+  //         container: '#editor',
+  //         fromElement: false,
+  //         plugins: [gjsPresetWebpage],
+  //         components: LandingPage.html || '<span><span/>',
+  //         style: LandingPage.css || '<></>',
+  //         storageManager: {
+  //           id: 'CMS-',
+  //           type: 'remote',
+  //           autosave: true,
+  //           autoload: false,
+  //           stepsBeforeSave: 1,
+  //           storeHtml: true,
+  //           storeCss: true,
+  //           urlStore: `http://localhost:8000/api/page/${router.query.pageSlug}`,
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //             credentials: true,
+  //             optionSuccessStatus: 200,
+  //           },
+  //         },
+
+  //       });
+
+  //       setEditor(e);
+  //     }
+  //   }
+  // };
+
+  const [{ data: getData }, refetchPageData] = useAxios(
+    {
+      url: `http://localhost:8000/api/page/${router.query.pageSlug}`,
+      method: 'GET',
+    },
+    {
+      manual: true,
+    },
+  );
+
+  const getApiM = () => {
+    console.log(router.query.pageSlug);
     if (router.query.pageSlug) {
-      axios.get(`http://localhost:8000/api/page/${router.query.pageSlug}`)
-        .then((res) => {
-          let obj = null;
+      refetchPageData().then((res) => {
+        console.log(res.data);
+        let obj = null;
 
-          let LandingPage = {};
-          if (router.query.pageSlug) {
-            obj = JSON.parse(res.data.data.data);
-            LandingPage = {
-              html: obj && obj['CMS-html'],
-              css: obj && obj['CMS-css'],
-              components: obj && obj['CMS-components'],
-              style: obj && obj['CMS-styles'],
-            };
-          }
+        let LandingPage = {};
+        // if (router.query.pageSlug) {
+        obj = JSON.parse(res.data.data.data);
+        LandingPage = {
+          html: obj && obj['CMS-html'],
+          css: obj && obj['CMS-css'],
+          components: obj && obj['CMS-components'],
+          style: obj && obj['CMS-styles'],
+        };
+        // }
 
-          if (!editor) {
-            const e = GrapesJS.init({
-              container: '#editor',
-              fromElement: false,
-              plugins: [gjsPresetWebpage],
-              components: LandingPage.html || '<span><span/>',
-              style: LandingPage.css || '<></>',
-              storageManager: {
-                id: 'CMS-',
-                type: 'remote',
-                autosave: true,
-                autoload: false,
-                stepsBeforeSave: 1,
-                storeHtml: true,
-                storeCss: true,
-                urlStore: `http://localhost:8000/api/page/${router.query.pageSlug}`,
-                headers: {
-                  'Content-Type': 'application/json',
-                  credentials: true,
-                  optionSuccessStatus: 200,
-                },
+        if (!editor) {
+          const e = GrapesJS.init({
+            container: '#editor',
+            fromElement: false,
+            plugins: [gjsPresetWebpage],
+            components: LandingPage.html || '<span><span/>',
+            style: LandingPage.css || '<></>',
+            storageManager: {
+              id: 'CMS-',
+              type: 'remote',
+              autosave: true,
+              autoload: false,
+              stepsBeforeSave: 1,
+              storeHtml: true,
+              storeCss: true,
+              urlStore: `http://localhost:8000/api/page/${router.query.pageSlug}`,
+              headers: {
+                'Content-Type': 'application/json',
+                credentials: true,
+                optionSuccessStatus: 200,
               },
+            },
 
-            });
+          });
 
-            setEditor(e);
-          }
-        });
+          setEditor(e);
+        }
+      });
     }
   };
 
   useEffect(() => {
-    getApi();
+    getApiM();
   }, [router.query.pageSlug]);
+
+  // const getApi = () => {
+  //   console.log(router.query.pageSlug);
+  //   if (router.query.pageSlug) {
+  //     axios.get(`http://localhost:8000/api/page/${router.query.pageSlug}`)
+  //       .then((res) => {
+  //         console.log(res.data);
+  //         let obj = null;
+
+  //         let LandingPage = {};
+  //         if (router.query.pageSlug) {
+  //           obj = JSON.parse(res.data.data.data);
+  //           LandingPage = {
+  //             html: obj && obj['CMS-html'],
+  //             css: obj && obj['CMS-css'],
+  //             components: obj && obj['CMS-components'],
+  //             style: obj && obj['CMS-styles'],
+  //           };
+  //         }
+
+  //         if (!editor) {
+  //           const e = GrapesJS.init({
+  //             container: '#editor',
+  //             fromElement: false,
+  //             plugins: [gjsPresetWebpage],
+  //             components: LandingPage.html || '<span><span/>',
+  //             style: LandingPage.css || '<></>',
+  //             storageManager: {
+  //               id: 'CMS-',
+  //               type: 'remote',
+  //               autosave: true,
+  //               autoload: false,
+  //               stepsBeforeSave: 1,
+  //               storeHtml: true,
+  //               storeCss: true,
+  //               urlStore: `http://localhost:8000/api/page/${router.query.pageSlug}`,
+  //               headers: {
+  //                 'Content-Type': 'application/json',
+  //                 credentials: true,
+  //                 optionSuccessStatus: 200,
+  //               },
+  //             },
+
+  //           });
+
+  //           setEditor(e);
+  //         }
+  //       });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getApi();
+  // }, [router.query.pageSlug]);
 
   return (
     <div>
