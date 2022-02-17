5d0d71d5c5c93387b9896665866bfc8d6b72af21
Sai Charan <saicharan@Sais-MacBook-Air.local>
2022-02-17 12:38:41 +0530

    change

--- ui/page-components/auth/PageSignin/index.js
+++ ui/page-components/auth/PageSignin/index.js
@@ -1,56 +1,69 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css';
-import { message } from 'antd';
+import {
+  message, Form, Input, Button, Row, Col, Typography,
+} from 'antd';
 import { useRouter } from 'next/router';
+import { MailOutlined, LockOutlined } from '@ant-design/icons';
 import styles from './style.module.scss';
 
-function PageSignin() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
-  const router = useRouter();
+const { Title, Paragraph } = Typography;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post('http://localhost:8000/api/auth/signin', { email, password })
+function PageSignin() {
+  const router = useRouter();
+  const onFinish = async (values) => {
+    await axios.post('http://localhost:8000/api/auth/signin', { email: values.email, password: values.password })
       .then(() => {
         router.push('/admin');
       })
       .catch(() => message.error('Invalid Signin, Please try again'));
   };
+
   const onSignUpClick = async () => {
     await router.push('/admin/signup');
   };
   return (
-    <div className={styles.body}>
-      <div className={styles.container}>
-        <div className={styles.overlay_container}>
-          <div className={styles.overlay}>
-            <div className={styles.overlay_panel}>
-              <h1 className={styles.h1}>Hello Machas!!</h1>
-              <p className={styles.p}>Enter Your Details and start Exploring</p>
-              <button type="submit" className={styles.ghost} id="signUp" onClick={onSignUpClick}>Sign Up</button>
-            </div>
-          </div>
-        </div>
-        <div className={styles.sign_in_container}>
-          <form
-            action=""
-            onSubmit={onFinish}
-            className={styles.form}
+    <Row>
+      <Col className={styles.overlay} span={12} style={{ backgroundColor: 'red', height: '100vh' }}>
+        <Typography style={{ textAlign: 'center' }}>
+          <Title font-family>Hello Rowdy Boys And Girls!!</Title>
+          <Paragraph style={{ color: 'white' }}>Enter Your Details and start Exploring</Paragraph>
+          <Button shape="round" size="large" onClick={onSignUpClick}>Sign Up</Button>
+        </Typography>
+      </Col>
+      <Col span={12} style={{ height: '100vh' }}>
+        <Form
+          name="normal_login"
+          className={styles.form}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Title>Sign In</Title>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please input your Email!' }]}
           >
-            <h1 className={styles.h1}>Sign In</h1>
-            <input type="email" name="email" id="email" placeholder="Email" className={styles.input} onChange={(e) => setEmail(e.target.value)} required />
-            <input type="password" name="password" id="password" placeholder="Password" className={styles.input} onChange={(e) => setPassword(e.target.value)} required />
-            <button type="submit" className={styles.button} onClick={handleSubmit}>Sign In</button>
-          </form>
-        </div>
-      </div>
-    </div>
-
+            <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="danger" shape="round" size="large" htmlType="submit" style={{ width: '200px', fontSize: '20px', lineHeight: 0.6 }}>
+              SIGN IN
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   );
 }
 
