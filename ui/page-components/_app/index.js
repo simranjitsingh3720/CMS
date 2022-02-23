54d09bc08376037a9e2ab52b5d32c288af894ac4
Aditya Phani <adityaphani@Adityas-MacBook-Air-2.local>
2022-02-23 12:34:13 +0530

    session

--- ui/page-components/_app/index.js
+++ ui/page-components/_app/index.js
@@ -47,7 +47,6 @@ CMSApp.getInitialProps = async ({ ctx }) => {
     };
   } else {
     const res = await axios.get('http://localhost:8000/api/user/me');
-    console.log(res.data);
     session = res.data;
   }
   return { session };
