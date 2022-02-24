2332adbb393ace6ea3f4f9fc87cac50e7d2b7cac
Aditya Phani <adityaphani@Adityas-MacBook-Air-2.local>
2022-02-24 18:56:52 +0530

    change in model

--- db/models/user.js
+++ db/models/user.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       models.User.hasOne(models.User, { foreignKey: 'updatedBy' });
       models.User.hasOne(models.User, { foreignKey: 'deletedBy' });
-      models.User.hasOne(models.Asset, { foreignKey: 'profilePicture' });
+      models.User.hasOne(models.Asset, { foreignKey: 'id' });
     }
   }
   User.init(
