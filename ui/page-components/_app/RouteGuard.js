54d773c207c09ceebef2bc840ee6be2507b505e9
Akshat Bhutra <akshat.bhutra@cogoport.com>
2022-02-26 10:17:26 +0530

    update

--- ui/page-components/_app/RouteGuard.js
+++ ui/page-components/_app/RouteGuard.js
@@ -5,7 +5,7 @@ function RouteGuard({ children, session }) {
   const router = useRouter();
   useEffect(() => {
     if (session.user) {
-      if (router.pathname.includes('/admin/signup') || router.pathname.includes('/admin/signin')) {
+      if (router.pathname.includes('/admin/signup') || router.pathname.includes('/admin/signin') || router.pathname.includes('/admin/password-recovery') || router.pathname.includes('/admin/password-change')) {
         router.replace('/admin', '/admin', { shallow: true });
       }
     } else if (!router.pathname.includes('/admin/signup') && !router.pathname.includes('/admin/signin') && !router.pathname.includes('/admin/password-recovery') && !router.pathname.includes('/admin/password-change')) {
