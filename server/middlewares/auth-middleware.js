c0bfd0f2309e81429bea8a7d1fb84f9f163a449e
Akshat Bhutra <akshat.bhutra@cogoport.com>
2022-03-03 12:11:16 +0530

    changes

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -1,21 +1,11 @@
 /* eslint-disable arrow-body-style */
 // import { NextResponse, NextRequest } from 'next/server';
-const { NextResponse, NextRequest } = require('next/server');
+// const { NextResponse, NextRequest } = require('next/server');
 
 const authMiddleware = (req, res, next) => {
-  if (!req.session.user) {
-    console.log('path ', req.path);
-    // if (req.path.includes('/signup') || req.path.includes('/signin') || req.path.includes('/forgot-password') || req.path.includes('/password-change')) {
-    //   // show the page
-    //   next();
-    // }
-    return next();
-  }
-  // if (req.path.includes('/signup') || req.path.includes('/signin') || req.path.includes('/forgot-password') || req.path.includes('/password-change')) {
-  // return next('/admin');
-  // }
   // show the page
   // next();
+  return next();
 };
 
 module.exports = authMiddleware;
