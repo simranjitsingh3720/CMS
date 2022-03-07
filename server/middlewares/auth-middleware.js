334a4bb8ee1c8ad7739969296b68e368c33e50e5
Akshat Bhutra <akshat.bhutra@cogoport.com>
2022-03-07 17:43:50 +0530

    change auth-middleware

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -1,25 +1,19 @@
 const authMiddleware = (req, res, next) => {
   const { path } = req;
-  console.log(path);
+  if (!path.startsWith('/admin')) {
+    return next();
+  }
   if (req.session.user) {
     if (path === '/admin/signin' || path === '/admin/signup' || path === '/admin/forgot-password') {
-      res.redirect('/admin');
+      return res.redirect('/admin');
     }
-  } else {
-    // if (path === '/admin/signin') {
-    //   // res.redirect('/admin/signin');
-    //   // res.redirect = '/admin/signin';
-    //   next();
-    // }
-
-    // if (path === '/admin/signup' || path === '/admin/forgot-password') {
-    //   next();
-    // }
-    next();
-    // res.redirect('/admin/signin');
-    // res.redirect = '/admin/signin';
   }
-  console.log('res ', res.redirect);
-  next();
+  if (!req.session.user) {
+    if (path !== '/admin/signin' && path !== '/admin/signup' && path !== '/admin/forgot-password') {
+      return res.redirect('/admin/signin');
+    }
+  }
+  return next();
 };
+
 module.exports = authMiddleware;
