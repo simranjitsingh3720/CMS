e479702a7a04f41e2007ea4d74ab5a9940d4f87f
Nagendra Singh <nagendra.singh@cogoport.com>
2022-03-09 12:52:31 +0530

    msg

--- ui/helpers/request-helper.js
+++ ui/helpers/request-helper.js
@@ -3,11 +3,15 @@ import { makeUseAxios } from 'axios-hooks';
 
 export const request = Axios.create({ baseURL: '/api/v1' });
 
-export const useRequest = makeUseAxios({
-  axios: request,
-  cache: false,
-  defaultOptions: {
-    ssr: true,
-    useCache: false,
+export const useRequest = makeUseAxios(
+  {
+    axios: request,
+    cache: false,
+    defaultOptions: {
+      ssr: true,
+      useCache: false,
+      manual: true,
+    },
   },
-});
+
+);
