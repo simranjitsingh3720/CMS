30ecb9a7226648b0c51f1fb193423f3a272fc5c6
simranjitsingh3720 <simranjit.singh@cogoport.com>
2022-03-29 16:03:02 +0530

    resolve add values issue

--- ui/page-components/dynamic-datastore/ContentBuilder/ShowSchema/StructureModal/apperanceComponent/ValueNames.js
+++ ui/page-components/dynamic-datastore/ContentBuilder/ShowSchema/StructureModal/apperanceComponent/ValueNames.js
@@ -1,77 +1,80 @@
 import { Form, Input, Button } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
-function ValueNames() {
+function ValueNames(fieldData) {
+  console.log('FIELD DATA ', fieldData.name);
+
   return (
-    <div>
-      <Form.List
-        name="values"
-        rules={[
-          {
-            validator: async (_, names) => {
-              if (!names || names.length < 2) {
-                return Promise.reject(new Error('At least 2 values'));
-              }
-            },
+
+    <Form.List
+      name="values"
+      rules={[
+        {
+          validator: async (_, names) => {
+            if (!names || names.length < 2) {
+              return Promise.reject(new Error('At least 2 values'));
+            }
           },
-        ]}
-      >
+        },
+      ]}
+    >
 
-        {(fields, { add, remove }, { errors }) => (
-          <>
-            {fields.map((field, index) => (
-              <Form.Item
-                label={index === 0 ? 'Value' : ''}
-                required={false}
-                key={field.key}
-              >
-                <Form.Item
-                  {...field}
-                  validateTrigger={['onChange', 'onBlur']}
-                  rules={[
-                    {
-                      required: true,
-                      whitespace: true,
-                      message: 'Please input values or delete this field.',
-                    },
-                  ]}
-                  noStyle
-                >
-                  <Input
-                    placeholder="Value"
-                    autoFocus
-                    style={{ width: '60%' }}
-                  />
-                </Form.Item>
-                {fields.length > 1 ? (
-                  <MinusCircleOutlined
-                    className="dynamic-delete-button"
-                    onClick={() => remove(field.name)}
-                  />
-                ) : null}
-              </Form.Item>
-            ))}
+      {(fields, { add, remove }, { errors }) => (
+        <>
+          {fields.map((field, index) => (
             <Form.Item
-              wrapperCol={{
-                offset: 7,
-                span: 10,
-              }}
+              label={index === 0 ? 'Value' : ''}
+              required={false}
+              key={field.key}
             >
-              <Button
-                type="dashed"
-                onClick={() => add()}
-                style={{ width: '60%' }}
-                icon={<PlusOutlined />}
+              <Form.Item
+                {...field}
+                validateTrigger={['onChange', 'onBlur']}
+                rules={[
+                  {
+                    required: true,
+                    whitespace: true,
+                    message: 'Please input values or delete this field.',
+                  },
+                ]}
+                noStyle
               >
-                Add Values
-              </Button>
-
-              <Form.ErrorList errors={errors} />
+                <Input
+                  placeholder="Value"
+                  autoFocus
+                  style={{ width: '60%' }}
+                />
+              </Form.Item>
+              {fields.length > 1 ? (
+                <MinusCircleOutlined
+                  className="dynamic-delete-button"
+                  onClick={() => remove(field.name)}
+                />
+              ) : null}
             </Form.Item>
-          </>
-        )}
-      </Form.List>
-    </div>
+          ))}
+          <Form.Item
+            wrapperCol={{
+              offset: 7,
+              span: 10,
+            }}
+          >
+            <Button
+              type="dashed"
+              onClick={() => add()}
+              style={{ width: '60%' }}
+              icon={<PlusOutlined />}
+            >
+              Add Values
+            </Button>
+
+            <Form.ErrorList errors={errors} />
+          </Form.Item>
+        </>
+      )}
+    </Form.List>
+  // </Form>
+
   );
 }
 
